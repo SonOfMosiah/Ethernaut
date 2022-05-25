846a163a9c5ffd93f9dd70f30ce7634a28e791ce
Ammon Werner <ammon@altafin.co>
2022-05-25 13:37:28 -0600

    Update King.sol

--- contracts/King.sol
+++ contracts/King.sol
@@ -25,17 +25,10 @@ contract King {
 }
 
 contract NewKing {
-    King public king = King(0x0754821630Fdf6E48b90B76473E0873dD9651950);
+    King public king = King(0x09E19236ce1bec192ac024539bE6591Bb2032e2F);
 
-    function claimKing() public {
-        address(king).call{value: 1000000000000000000}("");
-    }
-
-    fallback() external payable {
-        revert("Can't claim back the king spot lol");
-    }
-
-    receive() external payable {
-        revert("Can't claim back the king spot lol");
+    function claimKing() public payable {
+        (bool sent, ) = payable(king).call{value: msg.value}("");
+        require(sent, "Failed to send value!");
     }
 }
